a6d5bb2851ba2511370db0cd12cf0eb3eb6412c3
Bedi Szilvia <bedi.szilvia@gmail.com>
2016-11-09 21:04:42 +0100

    Monday updates

--- week-04/day-01/src/Workshop05arraylisttoarray.java
+++ week-04/day-01/src/Workshop05arraylisttoarray.java
@@ -8,18 +8,10 @@ public class Workshop05arraylisttoarray {
     public static void main(String... args) {
         ArrayList<Integer> numList = new ArrayList<Integer>(Arrays.asList(12, 1, 532, 23, 0, 7, 54, 32));
         // Copy every element of "numList" to an Array
-
-        Integer[] array = numList.toArray(new Integer[0]);
-
-        System.out.println(numList);
-
-        int[] result = new int[numList.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = numList.get(i);
-        }
-        System.out.println(numList);
+        Integer[] numListArray = (Integer[]) numList.toArray(new Integer[numList.size()]);
 
 
+            System.out.println(Arrays.toString(numListArray));
         }
     }
 
--- week-04/day-01/src/Workshop16.java
+++ week-04/day-01/src/Workshop16.java
@@ -9,29 +9,22 @@ public class Workshop16 {
     public static void main(String... args) {
         ArrayList<Integer> al = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
         // Fix this code fragment! It should remove every even number from the list.
-
-//        for (int i = 0, i < al.size(); i++) {
-//            Integer even = al.get(i) % 2;
-//            if (even == 0) {
-//                al.remove(even);
-//            }
-//            System.out.println(al);
-//        }
-
-        Iterator iterator = al.iterator();
-        while (iterator.hasNext()){
-            int element = (Integer) iterator.next();
-            if (element % 2 == 0) {
-                iterator.remove();
+        for (int element : al) {
+            int even = al.get(element) % 2;
+            if (even == 0) {
+                al.remove(element);
             }
+            System.out.println(al);
         }
-        System.out.println(al);
-
     }
 }
-//    Got the error below, why is that? Mert nem birja ebben a formaban.
+//    Got the error below, why is that?
 // Exception in thread "main" java.util.ConcurrentModificationException
 //        at java.util.ArrayList$Itr.checkForComodification(ArrayList.java:901)
 //        at java.util.ArrayList$Itr.next(ArrayList.java:851)
 //        at Workshop16.main(Workshop16.java:12)
-//
+//        at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+//        at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+//        at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+//        at java.lang.reflect.Method.invoke(Method.java:498)
+//        at com.intellij.rt.execution.application.AppMain.main(AppMain.java:147)
