30b4c3b5e9700df3abe512b237f23a960f547723
Bedi Szilvia <bedi.szilvia@gmail.com>
2016-11-09 20:31:59 +0100

    updates on week-4-Monday

--- week-04/day-01/src/Workshop05arraylisttoarray.java
+++ week-04/day-01/src/Workshop05arraylisttoarray.java
@@ -8,10 +8,18 @@ public class Workshop05arraylisttoarray {
     public static void main(String... args) {
         ArrayList<Integer> numList = new ArrayList<Integer>(Arrays.asList(12, 1, 532, 23, 0, 7, 54, 32));
         // Copy every element of "numList" to an Array
-        Integer[] numListArray = (Integer[]) numList.toArray(new Integer[numList.size()]);
+
+        Integer[] array = numList.toArray(new Integer[0]);
+
+        System.out.println(numList);
+
+        int[] result = new int[numList.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = numList.get(i);
+        }
+        System.out.println(numList);
 
 
-            System.out.println(Arrays.toString(numListArray));
         }
     }
 
---
+++ week-04/w4-day03/src/Workshop03.java
@@ -0,0 +1,49 @@
+
+/**************************************************************************************************
+ * Workshop: Practice With Exceptions: Arithmetic Overflow
+ * Write a routine that catches an arithmetic overflow (that is, larger than the Integer.MAX_VALUE)
+ * Note that Integer and int are NOT the same thing; and that Integer offers you properties such as
+ * MAX_VALUE.
+ * Use the Scanner routine to take the input from the keyboard; if the value is okay then
+ * print "Yum", and then 1/2 the value entered.  For example:-
+ * INPUT or 0 to end?  12334556677723131
+ * Invalid value
+ * (and the Java exception messages)
+ * INPUT or 0 to end?  12345678
+ * Yum 1234
+ * INPUT or 0 to end?  123456
+ * Yum 123
+ * INPUT or 0 to end? 0
+ * Bye!
+ **************************************************************************************************/
+
+import java.util.*;
+import java.util.Scanner; // Library that allows us to capture user input
+
+public class Workshop03 {
+
+    static Scanner userInput = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("INPUT or 0 to end? ");
+        int num = checkInputNum();
+
+        if (num != 0) {
+            String halfNumber = Integer.toString(num);
+            System.out.println("Yum " + halfNumber.substring(0,2));
+        }
+        else {
+            System.out.println("Bye!");
+        }
+
+    }
+    public static int checkInputNum() {
+        try {
+            return userInput.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Too High");
+            return 0;
+        }
+
+    }
+} //Workshop03
+
