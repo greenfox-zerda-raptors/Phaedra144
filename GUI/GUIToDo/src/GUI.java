5490264f8e624b40c1e5e06d74699c226c9254da
Bedi Szilvia <bedi.szilvia@gmail.com>
2016-11-25 13:19:58 +0100

    starts to connect GUI to APP

--- GUI/GUIToDo/src/GUI.java
+++ GUI/GUIToDo/src/GUI.java
@@ -15,42 +15,72 @@ public class GUI extends JFrame {
     private JButton remove;
     private JButton save;
     private JButton complete;
+    private JButton load;
 
 
     public GUI() {
         createFrame();
-        createMainPanel();
+        createPanels();
         addComponentsToMainPanel();
     }
 
     private void addComponentsToMainPanel() {
 
-        listingArea = new JTextArea("Your ToDo list is empty");
-        inputField = new JTextField("Write your new task here", 10);
+        inputField = new JTextField("Write your new task here", 20);
         inputField.setMaximumSize(new Dimension(450, 20));
+        inputField.setBorder(BorderFactory.createMatteBorder(0, 0, 20, 0, panel1.getBackground()));
+        listingArea = new JTextArea("Your ToDo list is empty");
+        listingArea.setMaximumSize(new Dimension(450, 400));
 
         panel1.add(inputField);
-        Box.createRigidArea(new Dimension(0, 20));
         panel1.add(listingArea);
+
+
+        add = new JButton("Add Task");
+        remove = new JButton("Remove Task");
+        complete = new JButton("Complete Task");
+        load = new JButton("Load Tasks");
+        save = new JButton("Save");
+
+        panel2.add(add);
+        panel2.add(remove);
+        panel2.add(complete);
+        panel2.add(load);
+        panel2.add(save);
+
+
 
 
     }
 
-    private void createMainPanel() {
+    private void createPanels () {
         panel1 = new JPanel();
-        panel1.setName("Main Panel");
-        panel1.setLayout(new BorderLayout());
-        frame1.add(panel1);
+        panel1.setName("Panel for textfields");
+        frame1.add(panel1, BorderLayout.NORTH);
         BoxLayout boxLayout = new BoxLayout(panel1, BoxLayout.Y_AXIS);
         panel1.setLayout(boxLayout);
-        panel1.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
+        panel1.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        panel2 = new JPanel();
+        panel2.setName("Panel for buttons");
+        frame1.add(panel2, BorderLayout.SOUTH);
+        panel2.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
+
     }
 
     private void createFrame() {
         frame1 = new JFrame("ToDo List");
-        frame1.setPreferredSize(new Dimension(500, 500));
-        frame1.setLocationRelativeTo(null);
-        frame1.setResizable(false);
+        BorderLayout myLayout = new BorderLayout(3,1);
+        frame1.setLayout(myLayout);
+
+        frame1.setPreferredSize(new Dimension(600, 600));
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension dim = tk.getScreenSize();
+        int xPos = (dim.width / 2) - (frame1.getWidth() / 2);
+        int yPos = (dim.height / 2) - (frame1.getHeight() / 2);
+        frame1.setLocation(xPos, yPos);
+
+//        frame1.setResizable(false);
         frame1.setVisible(true);
         frame1.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
