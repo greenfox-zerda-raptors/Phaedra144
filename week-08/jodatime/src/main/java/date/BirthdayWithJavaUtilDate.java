383d2f82936bbab6c26e2b1855af8455479d25fb
Bedi Szilvia <bedi.szilvia@gmail.com>
2016-12-13 15:52:14 +0100

    java util, first 3 methods done

--- week-08/jodatime/src/main/java/date/BirthdayWithJavaUtilDate.java
+++ week-08/jodatime/src/main/java/date/BirthdayWithJavaUtilDate.java
@@ -1,38 +1,74 @@
 package date;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public final class BirthdayWithJavaUtilDate implements BirthdayCalculator<Date> {
 
     @Override
     public Date parseDate(String str) {
         // TODO - return with the parsed date; format is: yyyy-MM-dd
-        return null;
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date result = null;
+        try {
+            result = sdf.parse(str);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     public String printMonthAndDay(Date date) {
         // TODO - return the date formatted: month & day (MM. dd.)
-        return null;
+        DateFormat df = new SimpleDateFormat("MM. dd.");
+
+        String reportDate = df.format(date);
+
+
+        return reportDate;
     }
 
     @Override
     public boolean isAnniversaryToday(Date date) {
         // TODO - return with true if today is the same month+day as date
-        return false;
+        Date today = new Date();
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.setTime(today);
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.setTime(date);
+//        boolean sameYear = calendar1.get(Calendar.YEAR) == calendar2.get(Calendar.YEAR);
+        boolean sameMonth = calendar1.get(Calendar.MONTH) == calendar2.get(Calendar.MONTH);
+        boolean sameDay = calendar1.get(Calendar.DAY_OF_MONTH) == calendar2.get(Calendar.DAY_OF_MONTH);
+        return (sameMonth && sameDay);
     }
 
     @Override
     public int calculateAgeInYears(Date birthday) {
         // TODO - return how many years age the input date 'birthday' was
-        return -1;
+        Date now = new Date();
+
+        long timeBetween = now.getTime() - birthday.getTime();
+        double yearsBetween = timeBetween / 3.156e+10;
+        int age = (int) Math.floor(yearsBetween);
+
+        return age;
     }
 
     @Override
     public int calculateDaysToNextAnniversary(Date date) {
         // TODO - the number of days remaining to the next anniversary of 'date' (e.g. if tomorrow, return 1)
-        return -1;
+        Date today = new Date();
+        long timeBetween = today.getTime() - date.getTime();
+        double daysBetween = timeBetween/86400000L - date.getYear()*366;
+        int days = (int) Math.floor(daysBetween);
+        return days;
     }
 
     public static void main(String[] args) {
